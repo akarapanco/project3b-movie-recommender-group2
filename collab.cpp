4f71c91212d094948a0525133ac02762357ff94e
Aleksia Karapanco <akarapanco@ufl.edu>
2025-11-25 12:40:53 -0500

    Update collab.cpp

--- collab.cpp
+++ collab.cpp
@@ -2,10 +2,14 @@
 #include <unordered_map>
 #include <vector>
 
-// Isa: implement Collaborative Filtering algorithm here
+
 std::vector<int> getCFRecommendations(
     int targetUserId,
-    const std::unordered_map<int, User>& users
-) {
-    // Isa: calculate similarity, predict ratings, return top movies
+    const std::unordered_map<int, User>& users {
+  
 }
+// ISA: This file is where you build the collaborative filtering system.
+// For the target user, compare their rating patterns to other users,
+// estimate what rating the target user would give to movies they haven't rated,
+// and then return the movie IDs with the strongest predicted ratings.
+// This should be a full second algorithm so we can compare it against KNN.
