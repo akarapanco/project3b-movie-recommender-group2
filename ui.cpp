0d88fe9195f290620394f07c029e4e7d7fd3c818
Aleksia Karapanco <akarapanco@ufl.edu>
2025-11-25 12:13:20 -0500

    Update ui.cpp

--- ui.cpp
+++ ui.cpp
@@ -3,6 +3,8 @@
 #include <unordered_map>
 #include "models.h"
 
+using namespace std;
+
 std::vector<Movie> loadMovies(const std::string& filename);
 std::unordered_map<int, User> loadUserRatings(const std::string& filename);
 std::vector<int> getRecommendations(
@@ -11,7 +13,11 @@ std::vector<int> getRecommendations(
     int k
 );
 
-using namespace std;
+// Isa: implement this function in collab.cpp
+std::vector<int> getCFRecommendations(
+    int targetUserId,
+    const std::unordered_map<int, User>& users
+);
 
 void showMenu() {
     cout << "\n=============================\n";
@@ -19,8 +25,9 @@ void showMenu() {
     cout << "=============================\n";
     cout << "1. List all movies\n";
     cout << "2. List all users\n";
-    cout << "3. Get recommendations for a user\n";
-    cout << "4. Exit\n";
+    cout << "3. Get KNN recommendations\n";
+    cout << "4. Get Collaborative Filtering recommendations\n";
+    cout << "5. Exit\n";
     cout << "Choose an option: ";
 }
 
@@ -35,11 +42,12 @@ int main() {
 
     int choice = 0;
 
-    while (choice != 4) {
+    while (choice != 5) {
         showMenu();
         cin >> choice;
 
         switch (choice) {
+
             case 1:
                 cout << "\n--- All Movies ---\n";
                 for (const Movie& m : movies) {
@@ -51,7 +59,7 @@ int main() {
                 cout << "\n--- All Users ---\n";
                 for (const auto& [uid, user] : users) {
                     cout << "User " << uid
-                         << " | Ratings: " << user.ratings.size() << "\n";
+                         << " | Ratings: " << user.ratings.size() << endl;
                 }
                 break;
 
@@ -69,7 +77,7 @@ int main() {
 
                 vector<int> recs = getRecommendations(userId, users, k);
 
-                cout << "\nRecommended movies:\n";
+                cout << "\nKNN Recommended Movies:\n";
                 for (int id : recs) {
                     for (const Movie& m : movies) {
                         if (m.id == id) {
@@ -80,7 +88,32 @@ int main() {
                 break;
             }
 
-            case 4:
+            case 4: {
+                int userId;
+                cout << "Enter user ID: ";
+                cin >> userId;
+
+                if (users.find(userId) == users.end()) {
+                    cout << "User not found!\n";
+                    break;
+                }
+
+                cout << "\nCollaborative Filtering Recommended Movies:\n";
+
+                // Isa: this will use your collaborative filtering algorithm
+                vector<int> recsCF = getCFRecommendations(userId, users);
+
+                for (int id : recsCF) {
+                    for (const Movie& m : movies) {
+                        if (m.id == id) {
+                            cout << m.title << " (" << m.genre << ")\n";
+                        }
+                    }
+                }
+                break;
+            }
+
+            case 5:
                 cout << "Goodbye!\n";
                 break;
 
