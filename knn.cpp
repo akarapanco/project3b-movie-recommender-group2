e70a21ed83cd5eaf4b03c82f3f625bfe245eecce
Aleksia Karapanco <akarapanco@ufl.edu>
2025-11-25 12:31:20 -0500

    Update knn.cpp

--- knn.cpp
+++ knn.cpp
@@ -1,5 +1,14 @@
+#include <vector>
+#include <unordered_map>
+#include "models.h"
+
+// ISA implement  getRecommendations() using KNN similarity algorithm
+
 std::vector<int> getRecommendations(
     int targetUserId,
     const std::unordered_map<int, User>& users,
     int k
-);
+) {
+    // ISA write full KNN algorithm here
+    return {}; 
+}
