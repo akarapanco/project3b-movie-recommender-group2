b64894ef9ffa8b623936c6277cf754608893a908
Aleksia Karapanco <akarapanco@ufl.edu>
2025-11-25 12:40:01 -0500

    Update knn.cpp

--- knn.cpp
+++ knn.cpp
@@ -2,13 +2,15 @@
 #include <unordered_map>
 #include "models.h"
 
-// ISA implement  getRecommendations() using KNN similarity algorithm
+// ISA: Your job in this file is to write the full KNN recommendation function.
+//     std::vector<int> getRecommendations(
+//         int targetUserId,
+//         const std::unordered_map<int, User>& users,
+//         int k
 
-std::vector<int> getRecommendations(
-    int targetUserId,
-    const std::unordered_map<int, User>& users,
-    int k
-) {
-    // ISA write full KNN algorithm here
-    return {}; 
-}
+
+// You will take the target user, measure how similar they are to every other user,
+// pick the closest users, look at the movies those users rated, and recommend
+// the movies the target user hasn’t seen yet. The function should return a list
+// of movie IDs that you think the target user would like based on similar users.
+
