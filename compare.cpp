a8fa79d7a0721a763f106bd603296109b451a16f
Aleksia Karapanco <akarapanco@ufl.edu>
2025-11-30 19:41:53 -0500

    Update compare.cpp

--- compare.cpp
+++ compare.cpp
@@ -1,37 +1,32 @@
+#include "models.h"
 #include <vector>
 #include <unordered_map>
-#include "models.h"
-#include "collab.h"
-#include "knn.h"
+#include <string>
+
 using namespace std;
+vector<int> getKNNRecommendations(int userId,
+                                  const unordered_map<int, User>& users,
+                                  const vector<Movie>& movies,
+                                  const string& preferredGenre);
 
-void compare_two_programs(unordered_map <int, User>& users, int k, int targetUserId)
+vector<int> getCFRecommendations(int userId,
+                                 const unordered_map<int, User>& users,
+                                 const vector<Movie>& movies,
+                                 const string& preferredGenre,
+                                 const vector<int>& excludeMovieIds);
+
+vector<int> getRecommendations(int userId,
+                               const unordered_map<int, User>& users,
+                               const vector<Movie>& movies,
+                               const string& preferredGenre,
+                               int choice,
+                               const vector<int>& exclude = {})
 {
-
-
-	vector <int> first = getCFRecommendations(targetUserId, users);
-	vector <int> second = getRecommendations(targetUserId, users, k);
-
-
-	cout << "Collaborative filter recommendation system below:" << endl;
-	for (int i = 1; i < (int)first.size()+1; i++)
-	{
-
-		cout <<i<< " Movie ID : " << first[i-1] <<endl;
-	}
-
-	cout << "KNN recommendation system below:" << endl;
-	for (int i = 1; i < (int)second.size() + 1; i++)
-	{
-
-		cout << i << " Movie ID : " << second[i-1] << endl;
-	}
-	cout << "Collaborative filtering recommendation system takes the absolute value of the difference between the ratings.";
-    cout <<"Then converts that difference into a score between 0 and 1."<<endl; 
-	cout << "KNN calculates using the square of the difference between ratings , averages those squared distances and takes the square root of the average";
+    if (choice == 1) {
+        return getKNNRecommendations(userId, users, movies, preferredGenre);
+    }
+    else if (choice == 2) {
+        return getCFRecommendations(userId, users, movies, preferredGenre, exclude);
+    }
+    return {}; 
 }
-
-// ISA: This file compares the two algorithms you wrote.
-// Call the KNN recommendations and your collaborative filtering recommendations,
-// print both lists for the same user, and describe how the results differ.
-// This will be used in our final report.
